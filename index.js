3b47d5b3035d8875de1d3f393ff07783bb86e247
Luisitodelcastillo <castilloluis166@gmail.com>
2023-05-17 19:05:22 +0200

    Visualitzador acabat

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-var xhr = new XMLHttpRequest();
 
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === this.DONE) {
-    console.log(JSON.parse(this.responseText));
-  }
+function getPokemonData(identifier) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${identifier}/`)
+    .then(response => response.json())
+    .then(data => {
+      const name = data.name;
+      const attack = data.stats[4].base_stat;
+      const hp = data.stats[5].base_stat;
+      const speed = data.stats[0].base_stat;
+      const imageUrl = data.sprites.front_default;
+
+      // Actualiza los elementos HTML con los nuevos datos
+      document.getElementById('pokemonName').textContent = name;
+      document.getElementById('attack').textContent = `Ataque: ${attack}`;
+      document.getElementById('hp').textContent = `Vida: ${hp}`;
+      document.getElementById('speed').textContent = `Velocidad: ${speed}`;
+      document.getElementById('pokemonImage').setAttribute('src', imageUrl);
+    })
+    .catch(error => {
+      // Manejo de error si no se encuentra el Pokémon
+      document.getElementById('pokemonName').textContent = 'No encontrado';
+      document.getElementById('attack').textContent = '';
+      document.getElementById('hp').textContent = '';
+      document.getElementById('speed').textContent = '';
+      document.getElementById('pokemonImage').setAttribute('src', 'pokemon-generic.png');
+    });
+}
+
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita el envío del formulario
+
+  const identifier = document.getElementById('searchInput').value; // Obtiene el valor del campo de entrada
+
+  // Llama a la función para obtener los datos del Pokémon
+  getPokemonData(identifier);
 });
-xhr.open("GET", "https://pokeapi.co/api/v2/pokemon/bulbasaur");
-xhr.send(null);
-54e
